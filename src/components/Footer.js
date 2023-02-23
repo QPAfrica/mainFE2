4dd1ebba93b0ad1fecd9ffc9df7f30760757b025
Arsalan Ahmad <you@example.com>
2023-02-23 06:31:13 +0500

    footer seeting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,10 @@
 import React from "react";
-import { BsTwitter, BsLinkedin } from "react-icons/bs";
 
 const Footer = ({ home }) => {
   return (
-    <div className={`${home ? "" : "bg-gray-900"}`}>
-      <footer class="p-5 container mx-auto rounded-lg shadow flex flex-col sm:flex-row items-center justify-between sm:p-6">
-        <span class="text-sm text-gray-300 pb-3 sm:pb-0 text-center">
-          All Rights Reserved © 2023{" "}
-          <a href="/" class="hover:underline">
-            QPAfrica.
-          </a>
-        </span>
-        <div class="flex items-center justify-center text-sm text-gray-300 sm:mt-0">
-          <a href="/" class="mr-4 hover:underline md:mr-6">
-            <BsLinkedin size={18} />
-          </a>
-          <a href="/" class="mr-4 hover:underline md:mr-6 ">
-            <BsTwitter size={18} />
-          </a>
-        </div>
+    <div className={`${home ? "absolute bottom-0 w-full" : ""} bg-gray-800`}>
+      <footer class="flex text-gray-300 justify-center py-5">
+        All Rights Reserved © 2023 QPAfrica.
       </footer>
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,33 +4,31 @@ import "../index.css";
 import "./home.css";
 import { TiArrowRightThick } from "react-icons/ti";
 import { GrFacebookOption, GrLinkedinOption, GrTwitter } from "react-icons/gr";
+import Footer from "../components/Footer";
 
 const home = () => {
   return (
     <div className="relative w-full App bg-gray-900 max-w-full">
       <div className="relative min-h-screen text-white">
-        <div className="circle" style={{ mixBlendMode: "blend" }}></div>
-        <div className="littlecircle" style={{ mixBlendMode: "blend" }}></div>
+        <div className="circle mix-blend-multiply"></div>
+        <div className="littlecircle mix-blend-multiply"></div>
         {/* <NavBar home={true} /> */}
         <div class="max-w-screen-lg mx-auto">
-          <div className="absolute top-[50%] translate-y-[-50%] container px-5">
-            <h1 class="text-4xl text-center sm:text-left sm:text-5xl w-full md:w-2/3 lg:w-1/2 p-4 font-bold text-white">
+          <div className="absolute top-[50%] translate-y-[-50%] container px-2 sm:px-5">
+            <h1 class="text-4xl text-center sm:text-left sm:text-5xl w-full md:w-2/3 lg:w-1/2 sm:p-4 font-bold text-white">
               Pioneering the Quant Industry in Africa
             </h1>
-            <div className="flex flex-row py-2 gap-6 items-center px-2">
-              <Link
-                to="fellows"
-                className="flex flex-row gap-1 items-center ml-2"
-              >
-                <p>QP Fellows</p>
+            <div className="flex flex-row py-4 gap-6 items-center justify-center sm:justify-start px-4">
+              <Link to="fellows" className="flex flex-row gap-1 items-center">
+                <p className="text-sm sm:text-base">QP Fellows</p>
                 <TiArrowRightThick className="rotate-[-45deg]" size={18} />
               </Link>
-              <Link to="" className="flex flex-row gap-1 items-center ml-2">
-                <p>QP Incubaded Funds</p>
+              <Link to="" className="flex flex-row gap-1 items-center">
+                <p className="text-sm sm:text-base">QP Incubaded Funds</p>
                 <TiArrowRightThick className="rotate-[-45deg]" size={18} />
               </Link>
             </div>
-            <div class="flex w-full md:w-2/3 lg:w-1/2 text-sm text-gray-300 p-4">
+            <div class="flex w-full md:w-2/3 lg:w-1/2 text-sm justify-center sm:justify-start text-gray-300 p-4">
               <a
                 href="/"
                 class="mr-4 hover:underline md:mr-6 bg-slate-400 rounded-md p-1"
@@ -52,8 +50,8 @@ const home = () => {
             </div>
           </div>
         </div>
-        {/* <Footer home={true} /> */}
       </div>
+      <Footer home={true} />
     </div>
   );
 };
