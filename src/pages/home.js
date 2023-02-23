4fc25e746c188d62294b6eabc6a4ca4326ad2b4f
Arsalan Ahmad <you@example.com>
2023-02-23 05:50:01 +0500

    home page animation

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { RiArrowRightUpFill } from "react-icons/ri";
-import Footer from "../components/Footer";
-import NavBar from "../components/NavBar";
 import { Link } from "react-router-dom";
 import "../index.css";
 import "./home.css";
